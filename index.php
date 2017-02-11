37feee680b5391b08fabc5ded1e1968cba7c6c3d
Jüri Ahhundov <jyriah@gmail.com>
2017-02-11 16:32:55 +0200

    esimene praktikum

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>Jüri kodukas</title>
+</head>
+<body>
+
+<p>Mingi vabsee suvakas tekst</p>
+
+<div>
+
+<?php
+echo "Tekst php echost!";
+?>
+
+<div>
+
+<img src="https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg" >
+
+</body>
+</html>
